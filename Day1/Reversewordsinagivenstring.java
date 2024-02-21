6a841fb0a194a8c712118f746d24da5e86d3a4d4
devkaushik2003 <114492351+devkaushik2003@users.noreply.github.com>
2024-02-21 19:04:13 +0530

    Create Reversewordsinagivenstring.java

---
+++ Day1/Reversewordsinagivenstring.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+    class GFG {
+        public static void main(String[] args) {
+    
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while (t > 0) {
+                String s = sc.next();
+                Solution obj = new Solution();
+                System.out.println(obj.reverseWords(s));
+                t--;
+            }
+        }
+    }
+    
+    // } Driver Code Ends
+    
+    
+    
+    class Solution 
+    {
+        //Function to reverse words in a given string.
+        String reverseWords(String S)
+        {
+            // code here
+            String result = "";
+            String[] arr = S.split("\\.");
+            for(int i = arr.length -1;i>=0 ; i--){
+                if(i == 0){
+                    result = result + arr[i];
+                    break;
+                }
+                result = result + arr[i] + ".";
+                
+            }
+            
+            return result;
+            
+            
+        }
+        
+    }
