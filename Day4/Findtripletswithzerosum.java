6c668e137615a61e462654d1a348b33d64823dea
devkaushik2003 <114492351+devkaushik2003@users.noreply.github.com>
2024-02-29 22:23:30 +0530

    Create Findtripletswithzerosum.java

---
+++ Day4/Findtripletswithzerosum.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+    import java.util.*;
+    class Triplets{
+        public static void main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            int t=sc.nextInt();
+            while(t-->0){
+                int n=sc.nextInt();
+                int[] a=new int[n];
+                for(int i=0;i<n;i++){
+                    a[i]=sc.nextInt();
+                }
+                Solution g=new Solution();
+                if(g.findTriplets(a,n))
+                    System.out.println("1");
+                else
+                    System.out.println("0");
+                
+            }
+        }
+    }
+    // } Driver Code Ends
+    
+    
+    /*Complete the function below*/
+    
+    
+    class Solution
+    {
+        // arr[]: input array
+        // n: size of the array
+        //Function to find triplets with zero sum.
+        public boolean findTriplets(int arr[] , int n)
+        {
+            //add code here.
+            Arrays.sort(arr);
+            boolean ans = false;
+            for(int i = 0;i<n;i++){
+                int l = i+1;
+                int r = n-1;
+                while(l<r){
+                    if(arr[i] + arr[l] + arr[r] == 0){
+                        l++;
+                        r--;
+                        ans = true;
+                        break;
+                    }else if(arr[i] + arr[l] + arr[r] < 0){
+                        l++;
+                        
+                    }else{
+                        r--;
+                    }
+                    if(ans){
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
