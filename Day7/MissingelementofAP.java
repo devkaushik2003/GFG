dee1c03b9c6b1abdce6e64c267b94ad60bbcbe29
devkaushik2003 <114492351+devkaushik2003@users.noreply.github.com>
2024-03-04 16:30:25 +0530

    Create MissingelementofAP.java

---
+++ Day7/MissingelementofAP.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMissing(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+    int findMissing(int[] arr, int n) {
+        // code here
+        int commonDiff = (arr[n-1]-arr[0])/n;
+        int low = 0;
+        int high = n-1;
+        
+        while(low < high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] == arr[0] + commonDiff*mid){
+                low = mid+1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return arr[0] + commonDiff*low;
+    }
+}
