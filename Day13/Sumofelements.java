d498abafb9974363489f6390e5ce2ca9ce4d34ed
devkaushik2003 <114492351+devkaushik2003@users.noreply.github.com>
2024-03-12 21:11:04 +0530

    Create Sumofelements.java

---
+++ Day13/Sumofelements.java
@@ -0,0 +1,57 @@
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            long k1 = Long.parseLong(stt.nextToken());
+            long k2 = Long.parseLong(stt.nextToken());
+            
+            Solution obj = new Solution();
+            System.out.println( obj.sumBetweenTwoKth(a, n, k1, k2) );
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution { 
+    public static long sumBetweenTwoKth(long A[], long N, long K1, long K2)
+    {
+        // Your code goes here
+        Arrays.sort(A);
+        long sum = 0;
+        int first = (int)K1;
+        int second = (int)K2;
+        for(int i = first ;i<second - 1;i++){
+            sum = sum + A[i];
+            
+        }
+        return sum;
+    }
+    
+}
