5f2e42034cdb77c401826f7d45430ed02ce05fb6
devkaushik2003 <114492351+devkaushik2003@users.noreply.github.com>
2024-03-17 19:05:11 +0530

    Create Addtwonumbers.java

---
+++ Day17/Addtwonumbers.java
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    
+  
+    String calc_Sum(int a[], int n, int b[], int m)
+    {
+        // Complete the function
+        int[] sum = new int[Math.max(n, m) + 1]; 
+        int c = 0;
+        int i = n - 1;
+        int j = m - 1;
+        int k = sum.length - 1;
+        
+        while (k >= 0) {
+            int d = c;
+            if (i >= 0) {
+                d += a[i];
+            }
+            if (j >= 0) {
+                d += b[j]; 
+            }
+            c = d / 10;
+            d = d % 10;
+            sum[k] = d;
+            i--;
+            k--;
+            j--;
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        boolean leadingZero = true; // Flag to track leading zeros
+        
+        for (int num : sum) {
+            if (num != 0) {
+                leadingZero = false; // Reset flag if non-zero digit encountered
+            }
+            if (!leadingZero) {
+                ans.append(num); // Append digit if not a leading zero
+            }
+        }
+        
+        if (ans.length() == 0) {
+            ans.append('0'); // If result is zero, append a single '0'
+        }
+        
+        return ans.toString();
+    }
+    
+  
+    
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray_N = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray_N];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray_N;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    String line2 = br.readLine();
+		    String[] ele = line2.trim().split("\\s+");
+		    int sizeOfArray_M = Integer.parseInt(ele[0]);
+		    
+		    int brr [] = new int[sizeOfArray_M];
+		    
+		    line2 = br.readLine();
+		    String[] elements2 = line2.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray_M;i++){
+		        brr[i] = Integer.parseInt(elements2[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		   
+		    String res = obj.calc_Sum(arr, sizeOfArray_N, brr, sizeOfArray_M);
+		    
+		    System.out.println(res);
+		    
+		    
+		}
+	}
+}
+
+
+            
+
+
+// } Driver Code Ends
