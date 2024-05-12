e263ea4b84781017330c78c01f9b2bb02d9b1727
devkaushik2003 <114492351+devkaushik2003@users.noreply.github.com>
2024-05-12 19:27:21 +0530

    Create Moveallzeros.java

---
+++ Day24/Moveallzeros.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+class Zeroes{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            Node head=null;
+            Node curr=null;
+            while(n-->0){
+                int a=sc.nextInt();
+                if(head==null){
+                    head=new Node(a);
+                    curr=head;
+                }
+                else{
+                    Node temp=new Node(a);
+                    temp.next=null;
+                    curr.next=temp;
+                    curr=temp;
+                }
+            }
+            GfG g=new GfG();
+            head = g.moveZeroes(head);
+            while(head!=null){
+                System.out.print(head.data+" ");
+                head=head.next;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+*/
+
+class GfG{
+    public Node moveZeroes(Node head){
+        //Your Code here.
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node node = head;
+        Node prev = null;
+        if(head.data == 0){
+            prev = node;
+            node = node.next;
+            
+        }
+        while(node != null){
+            if(node.data == 0){
+                prev.next = node.next;
+                node.next = head;
+                head = node;
+                node  = prev.next;
+            }else{
+                prev = node;
+                node = node.next;
+            }
+            
+        }
+        return head;
+        
+    }
+}
